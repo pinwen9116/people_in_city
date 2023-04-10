d395517763506aebb5045ed31683829b47ce99d6
pinwen9116 <pinwen2002@gmail.com>
2023-04-10 10:51:09 +0800

    10 times average

--- test.py
+++ test.py
@@ -28,20 +28,21 @@ def mean_x(xs, xs2):
     avg = 0
     for i in range(len(xs2)):
         xs2[i] /= (i+1)
-        avg += xs2[i]*(i+1)
+        avg += xs2[i] * (i+1)
     return statistics.mean(xs), avg / sum(xs2)
 
 ans_sum = 0
 res_sum = 0
 res2_sum = 0
-# 5 iter
+
+# 10 iter
+city, people, ans = test_case(10000) # suppose there are 10000 houses in town
 for i in range(10):
-    city, people, ans = test_case(10000) # suppose there are 1000 houses in town
     xs, xs2 = sample(people)
     res, res2 = mean_x(xs, xs2)
     ans_sum += ans
     res_sum += res
     res2_sum += res2
-    print(f"The correct answer is: {ans}, the estimated number is: {res}, the adjested number is: {res2}")
+    #print(f"The correct answer is: {ans}, the estimated number is: {res}, the adjested number is: {res2}")
 
-print(f"Average difference between:{(res_sum - ans_sum) / 10}, while adjusted difference is: {(res2_sum - ans_sum) / 5}")
+print(f"Average difference between:{(res_sum - ans_sum) / 10}, while adjusted difference is: {(res2_sum - ans_sum) / 10}")
